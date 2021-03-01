aaf1dd0b1f7e660a81f3466cf4b10a44e217e6ea
alcuritsin <al.curitsin@gmail.com>
2021-03-01 19:57:36 +0500

    is_palindrome (f)  + remove_symbol (f) (CW) --done
    is_palindrome (f)  + remove_symbol (f) (CW) --done

--- main.cpp
+++ main.cpp
@@ -2,13 +2,24 @@
  #include<Windows.h>
 //using namespace std;
 
-//TODO: 2021_02_24
+//TODO: 2021_02_26
 /*
 В проект CStrings добавить функции :
-void to_upper(char str[]);	//переводит строку в верхний регистр
-void to_lower(char str[]);	//переводит строку в нижний регистр
-void capitalize(char str[]);	//первую букву каждого слова в предложении делает заглвыной
-void shrink(char str[]);	//удаляет лишние пробелы из предложения, например: 
+bool is_int_number(char str[]);	//Определяет, является ли строка целым числом.
+int  to_int_number(char str[]);	//Если строка - целое число, то функция возвращает его значение.
+bool is_bin_number(char str[]);	//Определяет, является ли строка Двоичным числом.
+int  bin_to_dec(char str[]);	//Если строка - Двоичное число, то функция возвращает его десятичное значение.
+bool is_hex_number(char str[]);	//Определяет, является ли строка Шестнадцатеричным числом.
+int  hex_to_dec(char str[]);	//Если строка - Шестнадцатеричное число, то функция возвращает его десятичное значение.
+
+//DONE: 2021_02_24
+bool is_palindrome(char str[]);	//Определяет, является ли строка палиндромом. --done
+
+//DONE: 2021_02_24
+void to_upper(char str[]);	//переводит строку в верхний регистр --done
+void to_lower(char str[]);	//переводит строку в нижний регистр --done
+void capitalize(char str[]);	//первую букву каждого слова в предложении делает заглвыной --done
+void shrink(char str[]);	//удаляет лишние пробелы из предложения, например: --done
 				//Харашо      живет   на   свете          Винни Пух!
 				//Харашо живет на свете Винни Пух!
 */
@@ -22,6 +33,9 @@ void to_upper(char str[]);		//переводит строку в верхний 
 void to_lower(char str[]);		//переводит строку в нижний регистр;
 void capitalize(char str[]);	//первую букву каждого слова в предложении делает заглвыной;
 void shrink(char str[]);		//удаляет лишние пробелы из предложения;
+void remove_symbol(char str[], char symbol); //Удаляет все вхождения символа
+
+bool is_palindrome(char str[]);	//Определяет, является ли строка палиндромом.
 
 void main()
 {
@@ -46,24 +60,48 @@ void main()
 
 #ifdef HW_CSTRINGS_2021_02_24
 	//Проверочные строки:
-	//char str[] = " абвгд еёжз ийклм н опр стуф хцч шщ ъыь эюя ";
+	//char str[] = " абвгд е  ёжз ийклм н опр стуф хцч шщ ъыь эюя ";
 	//char str[] = " ab cd e f g h i gk l m o p q rst uv wz y x ";
-	char str[] = " Харашо      живет   на   свете          Винни Пух!";
+	//char str[] = " Харашо      живет   на   свете          Винни Пух!";
+	//char str[] = "123321";
+	//char str[] = "1234321";
+	//char str[] = "123456";
+	char str[] = "Аргентина манит негра";
+ 	
+	/*
+	const int n = 20;
+	char str[n];
 	
+	std::cout << "Введите строку :";
+	std::cin.getline(str, n);
+	*/
+
 	//Вывод в консоль начального варианта:
-	std::cout << "До реформации -- ";
+	//std::cout << "До реформации :";
 	std::cout << str << std::endl;
+	//std::cout<< "Длинна строки: " << StrLen(str) << std::endl;
 
 	//Трансформация:
 	//to_upper(str);	//переводит строку в верхний регистр;
 	//to_lower(str);	//переводит строку в нижний регистр;
 	//capitalize(str);	//первую букву каждого слова в предложении делает заглвыной;
-	shrink(str);		//удаляет лишние пробелы из предложения;
+	//shrink(str);		//удаляет лишние пробелы из предложения;
 
 	//Вывод результата:
-	std::cout << "После реформации -- ";
-	std::cout << str << std::endl;
+	//std::cout << "После реформации :";
+	//std::cout << str << std::endl;
 
+	//std::cout<< "Длинна строки: " << StrLen(str) << std::endl;
+
+	if (is_palindrome(str))
+	{
+		std::cout << "Строка палиндром!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Строка НЕ палиндром!" << std::endl;
+	}
+	std::cout << str << std::endl;
 
 #endif // HW_CSTRINGS_2021_02_24
 }
@@ -77,7 +115,6 @@ int StrLen(char str[])
 	int i = 0;
 	do 
 	{/*Ничиво не делаю :)*/} while (str[i++] != '\0');
-
 
 	return --i;
 }
@@ -111,7 +148,6 @@ en_US		a-z -> A-Z		97-122 -> 65-90		32
 ru_RU		а-я -> А-Я		224-255 -> 192-223	32
 ru_RU		ё -> Ё		241 -> 240				16
 */
-
 	int i = 0;
 	for (; str[i]; i++)
 	{
@@ -124,18 +160,39 @@ ru_RU		ё -> Ё		241 -> 240				16
 			str[i] += 16;
 		}
 	}
-
 }
 void capitalize(char str[])
 {//первую букву каждого слова в предложении делает заглвыной;
 
+	if (str[0] >= 'a' && str[0] <= 'z' || str[0] >= 'а' && str[0] <= 'я')
+	{//en_US		ru_RU
+		str[0] -= 32;
+	}
+	else if (str[0] == 'ё')
+	{//ru_RU
+	str[0] -= 16;
+	}
+
+	for (int i = 1; str[i]; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я') && str[i-1] ==' ')
+		{//en_US		ru_RU
+			str[i] -= 32;
+		}
+		else if (str[i] == 'ё' && str[i-1] == ' ')
+		{//ru_RU
+		str[i] -= 16;
+		}
+	}
+
+	/*
 	int i = 0;
 	//Первая буква в строке :)
 	if (str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'а' && str[i] <= 'я')
 	{//en_US		ru_RU
 		str[i] -= 32;
 	}
-		else if (str[i] == 'ё')
+	else if (str[i] == 'ё')
 	{//ru_RU
 	str[i] -= 16;
 	}
@@ -158,7 +215,7 @@ void capitalize(char str[])
 			}
 		}
 	}
-
+	*/
 }
 void shrink(char str[])
 {//удаляет лишние пробелы из предложения;
@@ -183,6 +240,42 @@ void shrink(char str[])
 			i--; //Возврат корретки :D
 		}
 	}
+}
+
+void remove_symbol(char str[], char symbol)
+{//Удаляет все вхождения символа
+	for (int i = 0; str[i]; i++)
+	{
+		while (str[i] == symbol)
+		{
+			for (int j = i; str[j]; j++) str[j] = str[j + 1];
+		}
+	}
+}
+
+bool is_palindrome(char str[])
+{//Определяет, является ли строка палиндромом.
+	int len = StrLen(str);
+
+	char* buffer = new char[len + 1]{};
+	
+	//for (int i = 0; i <= len; i++) buffer[i] = str[i];
+	std::strcpy(buffer, str); //String Copy. buffer - получатель; str - строка источник.
+
+	remove_symbol(buffer, ' ');
+	to_lower(buffer);
+
+	len = StrLen(buffer);
+	for (int i = 0; i<=len/2; i++)
+	{
+		if (buffer[i] != buffer[len - i - 1])
+		{
+			delete[] buffer;
+			return false;
+		}
+	}
+	delete[] buffer;
+	return true;
 }
 
 //Исполнитель
